75294aac46dec7515ff97c5c5db822be15039244
Qxyz17 <18859251107@163.com>
2025-09-19 22:09:35 +0800

    Update v1.0

---
+++ pack.bat
@@ -0,0 +1,5 @@
+@echo off
+python -m pip install -r requirements.txt
+python -m PyInstaller --onefile --icon=icon.ico -F android.py
+
+python -m PyInstaller -F web.py
---
+++ pack.sh
@@ -0,0 +1,4 @@
+python -m pip install -r requirements.txt
+python -m pyinstaller -F android.py
+
+python -m pyinstaller -F web.py
