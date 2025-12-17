cebed4266d6b615db099dcf5089772d90b3fbf9f
Qxyz17 <18859251107@163.com>
2025-12-17 17:14:07 +0800

    Fixed:修复编译时Qt5

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 python -m pip install -r requirements.txt
-python -m PyInstaller --onefile --windowed --icon=icon.ico 123pan.py
+python -m PyInstaller --onefile --windowed --icon=icon.ico --clean --exclude-module=PyQt5.QtWebEngine 123pan.py
