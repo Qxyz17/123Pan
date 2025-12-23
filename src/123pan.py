da1581273bb80348a4238b27e94a3adf2ecd7dd5
Qxyz17 <18859251107@163.com>
2025-12-23 21:41:07 +0800

    Add files via upload

--- src/123pan.py
+++ src/123pan.py
@@ -1696,3 +1696,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
---
+++ src/build.bat
@@ -0,0 +1,6 @@
+@echo off
+python -m pip install -r requirements.txt
+python -m PyInstaller --onefile --windowed --icon=icon.ico --clean --exclude-module=PyQt5.QtWebEngine 123pan.py
+
+
+
